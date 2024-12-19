c159b5492429e5ca6ae8abdcc42b899661b0422d
dakotab04 <dakotablanchard04@gmail.com>
2024-12-19 14:16:55 -0500

    Bullet implementation

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         #Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -16,6 +16,7 @@ class Settings:
 
         #Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #Alien settings
         self.alien_speed = 1.0
